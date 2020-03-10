aef7c58c6e2bc8109bd87f68ed7ec4ae46976f5f
jeremyyew <yewern@gmail.com>
2020-03-11 00:46:54 +0800

    Shift testcases into unittest methods.

--- jeremy-parser/tests.py
+++ jeremy-parser/tests.py
@@ -2,52 +2,8 @@ import pickle
 import unittest
 import deepdiff
 from terminals import LABEL_DOCUMENT
-from parser import preprocess, construct_node, check_arity
+from parser import preprocess, construct_node, check_arity, logger
 import utils
-
-
-coq_test = """
-Lemma A_1: forall n:nat, n = n.
-Proof.
-intro P H_P.
-Qed.
-"""
-
-coq_test1 = """
-Lemma A.
-Proof.
-intro n1.
-intro n2.
-intro n3.
-exact (lemma_a_1).
-exact (lemma_a_1 a1 a2).
-exact (lemma_b_2).
-exact (lemma_b_2 b1).
-exact (lemma_b_2 b1 b2 b3).
-exact (lemma_b_2 (lemma_a_1)).
-exact (lemma_b_2 b1 (lemma_a_1)).
-
-exact (lemma_a_1 a1).
-exact (lemma_b_2 b1 b2).
-exact (lemma_b_2 (lemma_a_1 a1) (lemma_b_2 b1 b2)).
-Qed.
-Lemma B.
-"""
-
-coq_test2 = """
-Proof.
-intro n1.
-intro n2.
-intro n3.
-exact (lemma_b_2 (lemma_a_1 a1) (lemma_b_2 b1 b2)).
-Qed.
-"""
-
-coq_test_arity1 = """
-Proof.
-    exact (lemma_b_2 (lemma_a_1)).
-Qed.
-"""
 
 
 class TestParser(unittest.TestCase):
@@ -56,16 +12,18 @@ class TestParser(unittest.TestCase):
         t = construct_node(s, LABEL_DOCUMENT)
 
         try:
-            with open(f'pickled_trees/{name}', 'rb') as f:
+            with open(f'pickled/{name}', 'rb') as f:
                 pickled = pickle.load(f)
         except FileNotFoundError:
-            pickle.dump(t, open(f'pickled_trees/{name}', 'wb'))
-            with open(f'pickled_trees/{name}', 'rb') as f:
+            with open(f'pickled/{name}', 'wb') as f:
+                pickle.dump(t, f)
+            with open(f'pickled/{name}', 'rb') as f:
                 pickled = pickle.load(f)
 
         diff = deepdiff.DeepDiff(t, pickled)
         try:
             self.assertEqual(diff, {})
+            utils.pretty(t)
         except AssertionError:
             print(
                 f"\n*********\nPARSER OUTPUT FOR TESTCASE: {name}\n*********\n")
@@ -74,30 +32,85 @@ class TestParser(unittest.TestCase):
                 f"\n*********\nEXPECTED FOR TESTCASE: {name}\n*********\n")
             utils.pretty(pickled)
 
-    def arity_helper(self, name, code, arity_db):
+    def test_assertion1(self):
+        assertion1 = """
+        Lemma A_1 :
+        forall a b : nat,
+        a + b = a + b.
+        Lemma A_1 :
+        forall a b : nat,
+        a + b = a + b.
+        Lemma A_1 :
+        forall a b : nat,
+        a + b = a + b.
+        """
+        self.parser_helper("assertion1", assertion1)
+
+    def test_term1(self):
+        term1 = """Proof.
+        intro n1.
+        intro n2.
+        intro n3.
+        exact (lemma_b_2 (lemma_a_1 a1) (lemma_b_2 b1 b2)).
+        Qed.
+        """
+        self.parser_helper("term1", term1)
+
+
+class TestParityCheck(unittest.TestCase):
+    def arity_helper(self, code, arity_db, expected_warnings):
         s = preprocess(code)
         t = construct_node(s, LABEL_DOCUMENT)
         warnings = check_arity(t, arity_db, None)
         self.assertEqual(warnings,
-                         [('lemma_b_2 (lemma_a_1)',
-                           'lemma_b_2', 2, 1, '(lemma_a_1)'),
-                          ('lemma_b_2 (lemma_a_1)',
-                           'lemma_a_1', 1, 0, '')])
+                         expected_warnings)
 
-    def test_parser(self):
-        self.parser_helper("coq_test2", coq_test2)
-
-    def test_arity(self):
+    def test_arity_pos(self):
         arity_db = {
             "lemma_a_1": 1,
-            "lemma_b_2": 2,
+            "lemma_a_2": 2,
+            "lemma_a_3": 3,
         }
-        self.arity_helper("coq_test_arity1", coq_test_arity1, arity_db)
+        test = """Proof.
+        exact (lemma_a_1 n1).
+        exact (lemma_a_2 n1 n2).
+        exact (lemma_a_3 n1 n2 n3).
+        exact (lemma_a_3 (lemma_a_2 n1 n2) (lemma_a_1 n1) n3).
+        Qed.
+        """
+        expected_warnings = []
+        self.arity_helper(test,
+                          arity_db, expected_warnings)
 
-
-class TestParityCheck(unittest.TestCase):
-    def test_arity(self):
-        pass
+    def test_arity_neg(self):
+        arity_db = {
+            "lemma_a_1": 1,
+            "lemma_a_2": 2,
+        }
+        test = """Proof.
+        exact (lemma_a_1).
+        exact (lemma_a_1 n1 n2).
+        exact (lemma_a_2).
+        exact (lemma_a_2 n1).
+        exact (lemma_a_2 n1 n2 n3).
+        exact (lemma_a_2 (lemma_a_1)).
+        exact (lemma_a_2 (lemma_a_1 n1 n2) n3 n4).
+        Qed.
+        """
+        # parent, first_term, arity_expected, arity, arg_strings
+        expected_warnings = [
+            ('lemma_a_1', 'lemma_a_1', 1, 0, ''),
+            ('lemma_a_1 n1 n2', 'lemma_a_1', 1, 2, '(n1),(n2)'),
+            ('lemma_a_2', 'lemma_a_2', 2, 0, ''),
+            ('lemma_a_2 n1', 'lemma_a_2', 2, 1, '(n1)'),
+            ('lemma_a_2 n1 n2 n3', 'lemma_a_2', 2, 3, '(n1),(n2),(n3)'),
+            ('lemma_a_2 (lemma_a_1)', 'lemma_a_2', 2, 1, '(lemma_a_1)'),
+            ('lemma_a_2 (lemma_a_1)', 'lemma_a_1', 1, 0, ''),
+            ('lemma_a_2 (lemma_a_1 n1 n2) n3 n4', 'lemma_a_2',
+             2, 3, '(lemma_a_1 n1 n2),(n3),(n4)'),
+            ('lemma_a_1 n1 n2', 'lemma_a_1', 1, 2, '(n1),(n2)')]
+        self.arity_helper(test,
+                          arity_db, expected_warnings)
 
 
 unittest.main()
