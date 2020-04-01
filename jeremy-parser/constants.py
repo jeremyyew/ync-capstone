06278bb91bd60d675db59818bbd4b409d2e7e99d
jeremyyew <yewern@gmail.com>
2020-04-01 10:46:45 +0800

    Alphabetize, add LTAC, refactor DOC_LOOKAHEAD.

--- jeremy-parser/constants.py
+++ jeremy-parser/constants.py
@@ -2,16 +2,17 @@ import re
 
 # LABEL CONSTANTS.
 # Toplevel labels.
-LABEL_DOCUMENT = "DOCUMENT"
-LABEL_REQUIRE_IMPORT = "REQUIRE_IMPORT"
-LABEL_PROOF = "PROOF"
 LABEL_ASSERTION = "ASSERTION"
+LABEL_DOCUMENT = "DOCUMENT"
+LABEL_LTAC = "LTAC"
+LABEL_PROOF = "PROOF"
+LABEL_REQUIRE_IMPORT = "REQUIRE_IMPORT"
 
 # Toplevel subcomponent labels.
+LABEL_ASSERTION_IDENT = "ASSERTION_IDENT"
 LABEL_ASSERTION_KEYWORD = "ASSERTION_KEYWORD"
 LABEL_ASSERTION_TERM = "ASSERTION_TERM"
 LABEL_FORALL = "FORALL"
-LABEL_ASSERTION_IDENT = "ASSERTION_IDENT"
 
 # Tactic command labels (alphabetically sorted).
 LABEL_APPLY = "APPLY"
@@ -32,17 +33,19 @@ LABEL_SPLIT = "SPLIT"
 LABEL_SYMMETRY = "SYMMETRY"
 
 # Tactic subcomponent labels.
+LABEL_BINDER = "BINDER"
 LABEL_REWRITE_ARROW = "REWRITE_ARROW"
 LABEL_TERM = "TERM"
 LABEL_TYPE = "TYPE"
-LABEL_BINDER = "BINDER"
 
 # KEYWORD CONSTANTS.
 # Toplevel keywords.
+KW_ABORT = "Abort"
+KW_ADMITTED = "Admitted"
+KW_LTAC = "Ltac"
 KW_PROOF = "Proof"
 KW_QED = "Qed"
-KW_ADMITTED = "Admitted"
-KW_ABORT = "Abort"
+KW_REQUIRE_IMPORT = "Require Import"
 
 # Assertion and subcomponent keywords (alphabetically sorted).
 KW_COROLLARY = "Corollary"
@@ -73,7 +76,7 @@ KW_SPLIT = "split"
 KW_SYMMETRY = "symmetry"
 
 # REGULAR EXPRESSION CONSTANTS (need to be included in keyword groups, so defined first).
-REGEXP_COMMENT = r"\(\*.+?\*\)"
+REGEXP_COMMENT = r"\s*\(\*.+?\*\)\s*"
 REGEXP_BULLET = r"[\+\-\*]+\s*"
 
 # KEYWORD GROUPS.
@@ -96,12 +99,6 @@ KW_GRP_TACTIC = [
     REGEXP_COMMENT,
 ]
 
-KW_GRP_DOCUMENT = [
-    KW_PROOF,
-    KW_CHECK,
-    KW_COMPUTE
-]
-
 KW_GRP_ASSERTION = [
     KW_COROLLARY,
     KW_DEFINITION,
@@ -114,6 +111,15 @@ KW_GRP_ASSERTION = [
     KW_THEOREM,
 ]
 
+KW_GRP_DOCUMENT = [
+    KW_CHECK,
+    KW_COMPUTE,
+    KW_LTAC,
+    KW_PROOF,
+    KW_REQUIRE_IMPORT,
+    REGEXP_COMMENT,
+] + KW_GRP_ASSERTION
+
 # REGULAR EXPRESSION CONSTANTS.
 
 
@@ -121,13 +127,15 @@ def regx_non_capture_alt(grp):
     return f"(?:{'|'.join(grp)})"
 
 
+REGEXP_ASSERTION = regx_non_capture_alt(KW_GRP_ASSERTION)
+REGEXP_DOCUMENT = regx_non_capture_alt(KW_GRP_DOCUMENT)
 REGEXP_TACTIC_END = r"[\.;]"
 REGEXP_TACTIC = regx_non_capture_alt(KW_GRP_TACTIC)
 REGEXP_TACTIC_LOOKAHEAD = f"(?={REGEXP_TACTIC}|$)"
-REGEXP_DOCUMENT = regx_non_capture_alt(KW_GRP_DOCUMENT)
-REGEXP_ASSERTION = regx_non_capture_alt(KW_GRP_ASSERTION)
 REGEXP_OR_AND_INTROPATTERN = r"(?:\[[^\.]+?\]|\S+?)"
 REGEXP_IN_OCCURRENCE = r"(?:\sin\s\S+?)?"
 REGEXP_AT_OCCURRENCE = r"(?:\sat(?:\s\d)+?)?"
 REGEXP_AS_INTROPATTERN = fr"(?:\s?as\s?{REGEXP_OR_AND_INTROPATTERN})?"
 REGEXP_USING_TERM = fr"(?:\susing\s\S+?)?"
+
+REGEXP_DOC_LOOKAHEAD = f"(?={REGEXP_TACTIC}|{REGEXP_DOCUMENT}|$)"
